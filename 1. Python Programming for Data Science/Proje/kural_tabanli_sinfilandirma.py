3e4838bc94e6cc31058d209f07ba598a512ec5f5
Merve <vier-und.achtzig-tm@hotmail.com>
2022-12-08 10:38:23 +0300

    Update kural_tabanli_sinfilandirma.py

--- 1. Python Programming for Data Science/Proje/kural_tabanli_sinfilandirma.py
+++ 1. Python Programming for Data Science/Proje/kural_tabanli_sinfilandirma.py
@@ -2,7 +2,7 @@
 # Kural Tabanlı Sınıflandırma ile Potansiyel Müşteri Getirisi Hesaplama
 
 
-# İş Problemi
+# İş Problemi 
 """Bir oyun şirketi müşterilerinin bazı özelliklerini kullanarak
 seviye tabanlı (level based) yeni müşteri tanımları (persona)
 oluşturmak ve bu yeni müşteri tanımlarına göre segmentler
